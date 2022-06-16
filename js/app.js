836ba9c7ace4b05f4f285da03611f23f00834066
May Carandang <maytiara@gmail.com>
2022-06-17 01:58:16 +0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+
+
+// start the timer
+
+
+// display a question - starting with 1st ques
+// handle question click - check whether the ans is correct or wrong
+
+// end the quiz and show highscores
+
+//Still working on the code.. apologies for being slow 🐢
+//I'll create multiple commits after this
+
+
+
+
